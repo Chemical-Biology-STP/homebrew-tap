9814d15e08ccb80a5ce809e438a9593936254e73
Yip Yew Mun <yipy0005@gmail.com>
2026-02-12 12:31:27 +0000

    chore: update time-tracker-companion SHA for v1.6.2 rebuild

--- Casks/time-tracker-companion.rb
+++ Casks/time-tracker-companion.rb
@@ -1,6 +1,6 @@
 cask "time-tracker-companion" do
   version "1.6.2"
-  sha256 "0521bf14d9d0210daa5b97f64786701cd524d88babde4e772e8065f14c347342"
+  sha256 "72bbda143f8c95d65b8cefbcc6c00baf85812e980c5479d53618574678edfe62"
 
   url "https://github.com/Chemical-Biology-STP/time_tracker/releases/download/v#{version}/TimeTrackerCompanion.dmg"
   name "Time Tracker Companion"
